3567f22c9c838cb74394e2a437fb07731f8d9d04
rishichawda <chawda.rishii@gmail.com>
2020-10-21 10:02:04 +0530

    add node-scaffolding

---
+++ node-scaffolding/plan.sh
@@ -0,0 +1,19 @@
+pkg_name=node-scaffolding
+pkg_origin=rishichawda
+pkg_version="0.6.14"
+pkg_maintainer="Rishi Chawda"
+pkg_license=('Apache-2.0')
+pkg_description="Habitat Plan Scaffolding for Node.js Applications"
+pkg_upstream_url="https://github.com/habitat-sh/core-plans/tree/master/scaffolding-node"
+pkg_deps=(core/tar core/rq core/jq-static core/gawk core/curl core/bc core/coreutils)
+pkg_build_deps=(core/node/12.14.1 core/coreutils core/yarn)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  find lib -type f | while read -r f; do
+    install -D -m 0644 "$f" "$pkg_prefix/$f"
+  done
+}
